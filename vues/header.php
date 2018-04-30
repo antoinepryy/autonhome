106db961920b7bc6fc8dd6c64beb20df5dac78a4
Antoine <antoine.ap.57@gmail.com>
2018-04-30 15:07:11 +0200

    debug css

--- vues/header.php
+++ vues/header.php
@@ -8,15 +8,15 @@
     <title>Autonhome</title>
     <meta charset="utf-8"/>
     <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="CSS/header.css"/>
-    <link rel="stylesheet" type="text/css" href="CSS/footer.css"/>
-    <link rel="stylesheet" type="text/css" href="CSS/accueil.css"/>
+    <link rel="stylesheet" type="text/css" href="vues/CSS/header.css"/>
+    <link rel="stylesheet" type="text/css" href="vues/CSS/footer.css"/>
+    <link rel="stylesheet" type="text/css" href="vues/CSS/accueil.css"/>
 
 </head>
 <body>
 
 <header>
-    <div class="footer-title">
+    <div class="header-title">
         <a href=""><img src="ressources/logo/logo.png"/></a>
 
     </div>
