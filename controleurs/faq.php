4604a346bcb1bc55cf90d791bfd5854d8fd135e7
Antoine <antoine.ap.57@gmail.com>
2018-04-30 15:25:53 +0200

    Ajout controleurs footer links

---
+++ controleurs/faq.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antoine
+ * Date: 30/04/2018
+ * Time: 15:21
+ */
+
+include('vues/faq.php');
---
+++ controleurs/mentionslegales.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antoine
+ * Date: 30/04/2018
+ * Time: 15:20
+ */
+
+include('vues/mentionslegales.php');
