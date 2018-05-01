65522a7e494b40cd1b87db0345d3c0e32a3feb24
Nigirith <fanny.streiff@gmail.com>
2018-05-01 02:25:19 +0200

    ajout page header utilisateur connecte

--- vues/header_LU.php
+++ vues/header_LU.php
@@ -8,7 +8,7 @@
     <title>Autonhome</title>
     <meta charset="utf-8"/>
     <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="vues/CSS/header.css"/>
+    <link rel="stylesheet" type="text/css" href="vues/CSS/header_LU.css"/>
     <link rel="stylesheet" type="text/css" href="vues/CSS/footer.css"/>
     <link rel="stylesheet" type="text/css" href="vues/CSS/accueil.css"/>
 
@@ -16,9 +16,14 @@
 <body>
 
 <header>
+
+    <div class="ma-maison">
+        <a href=""> Ma maison </a>
+    </div>
+
+
     <div class="header-title">
         <a href="index.php?cible=accueil"><img src="ressources/logo/logodomisep_logo.png"/></a>
         <a href="index.php?cible=accueil"><img src="ressources/logo/logodomisep_logo_texte.png"/></a>
 
     </div>
-    <div>
