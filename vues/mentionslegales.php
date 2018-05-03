d6b8375ce67ae9ad3b320a8f61386f7d0a77c3a2
ameliep18 <memainef@gmail.com>
2018-05-03 11:32:01 +0200

    Mentions légales suite et fin

--- vues/mentionslegales.php
+++ vues/mentionslegales.php
@@ -11,53 +11,66 @@ require ('vues/header.php');
 
 <section>
 
-    <h1> Mentions légales</h1>
-    <p>Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004
+    </br>
+    <h1 class="title-mentionslegales"> Mentions légales</h1>
+    </br></br>
+    <p class="intro">Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004
     pour la Confiance dans l&#39;économie numérique, dite L.C.E.N., il est porté à la connaissance des
     utilisateurs et visiteurs du site AutonHome.DomIsep.com les présentes mentions légales.
-    Le site AutonHome.DomIsep.com est accessible à l&#39;adresse suivante :
-    http://www.AutonHome.DomIsep.com (ci-après « le Site »). L&#39;accès et l&#39;utilisation du Site
+    </br>Le site AutonHome.DomIsep.com est accessible à l&#39;adresse suivante :
+        http://www.AutonHome.DomIsep.com (ci-après « le Site »). </br> L&#39;accès et l&#39;utilisation du Site
     sont soumis aux présentes &quot; Mentions légales&quot; détaillées ci-après ainsi qu&#39;aux lois et/ou
     règlements applicables.
-    La connexion, l&#39;utilisation et l&#39;accès à ce Site impliquent l&#39;acceptation intégrale et sans réserve
+    </br>La connexion, l&#39;utilisation et l&#39;accès à ce Site impliquent l&#39;acceptation intégrale et sans réserve
         de l&#39;internaute de toutes les dispositions des présentes Mentions Légales.</p>
+    </br></br>
 
     <h3>ARTICLE 1 – Informations légales</h3>
-    <p>En vertu de l&#39;Article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans
+    </br>
+    <p class="article1" >En vertu de l&#39;Article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans
     l&#39;économie numérique, il est précisé dans cet article l&#39;identité des différents intervenants dans
     le cadre de sa réalisation et de son suivi.
     Le Site est édité par YMPIsep ayant son siège social à l&#39;adresse suivante : 28 rue Notre-Dame-
     des-Champs, 75006 Paris ; Adresse e-mail : ympisep@gmail.com (ci-après « l’Editeur »).
     Sont considérés comme utilisateurs tous les internautes qui naviguent, lisent, visionnent et
     utilisent le Site (ci-après « les Utilisateurs »).</p>
+    </br></br>
 
     <h3>ARTICLE 2 – Accessibilité</h3>
-    <p>Le Site est par principe accessible aux Utilisateurs 24/24h et 7/7j, sauf interruption,
+    </br>
+    <p class="article2" >Le Site est par principe accessible aux Utilisateurs 24/24h et 7/7j, sauf interruption,
     programmée ou non, pour des besoins de maintenance ou en cas de force majeure.
     En cas d&#39;impossibilité d&#39;accès au Site, celui-ci s&#39;engage à faire son maximum afin d&#39;en rétablir
     l&#39;accès. Le Site ne saurait être tenu pour responsable de tout dommage, quelle qu&#39;en soit la
         nature, résultant de son indisponibilité.</p>
+    </br></br>
 
     <h3>ARTICLE 3 – Collecte des données et loi informatiques et libertés</h3>
-    <p>Ce site est déclaré à la Commission Nationale Informatique et Libertés (CNIL).
+    </br>
+    <p class="article3" >Ce site est déclaré à la Commission Nationale Informatique et Libertés (CNIL).
     En outre, il est conforme aux dispositions de la Loi 78-17 du 6 janvier 1978 relative à
     l&#39;informatique, aux fichiers et aux libertés. En vertu de celle-ci l&#39;Utilisateur bénéficie
     notamment d&#39;un droit d&#39;opposition (art. 32 et 38), d&#39;accès (art. 38 et 39) et de rectification (art.
         40) des données le concernant.</p>
+    </br></br>
 
     <h3>ARTICLE 4 – Politique de cookies</h3>
-    <p>Le Site a éventuellement recours aux techniques de &quot;cookies&quot; lui permettant de traiter des
+    </br>
+    <p class="article4"> Le Site a éventuellement recours aux techniques de &quot;cookies&quot; lui permettant de traiter des
     statistiques et des informations sur le trafic, de faciliter la navigation et d&#39;améliorer le service
     pour le confort de l&#39;Utilisateur.
     Conformément à la législation européenne, le Site a mis à jour sa politique de confidentialité
     en matière de cookies. L&#39;Utilisateur est libre d&#39;accepter ou de refuser les cookies de tous les
         sites internet en modifiant les paramètres de son navigateur internet.</p>
+    </br></br>
 
     <h3>ARTICLE 5 – Contact</h3>
-    <p>Pour tout signalement de contenus ou d&#39;activités illicites, l&#39;Utilisateur peut contacter le
+    </br>
+    <p class="article5"> Pour tout signalement de contenus ou d&#39;activités illicites, l&#39;Utilisateur peut contacter le
     Directeur de publication à l&#39;adresse suivante : autonhome@gmail.com, ou par courrier
     recommandé avec accusé de réception adressé à l&#39;Éditeur aux coordonnées précisées dans les
         présentes mentions légales.</p>
+    </br></br></br>
 
 </section>
 
