9955f252d555e7c71f3b17b2b9feb4bffefe6518
Antoine <antoine.ap.57@gmail.com>
2018-05-04 12:08:13 +0200

    before manger

--- controleurs/accueil.php
+++ controleurs/accueil.php
@@ -6,5 +6,13 @@
  * Time: 18:40
  */
 $section = 'accueil';
+require ('sessions/user.php');
+
+auth(array(
+    'mail' => 'test',
+    'firstName' => 'test',
+    'lastName' => 'test',
+    'ID' => 23,
+));
 
 include('vues/accueil.php');
