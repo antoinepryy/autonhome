de3f6f870da72853b101f5c67e6c8c1dce578916
ameliep18 <memainef@gmail.com>
2018-05-04 14:57:32 +0200

    Mentions légales et A propos

--- vues/apropos.php
+++ vues/apropos.php
@@ -9,7 +9,19 @@
 require ('vues/header.php');
 
 ?>
+<section>
+    <br/>
+    <h1> À propos </h1>
+    <div id="apropos">
+        <img class="logo" src="ressources/logo/domisep.GIF" alt="logo domisep"/>
+        <p class="text"> Autonhome est un service proposé par la sociéte Domisep, fondée en 2002. </br>
+            Pour plus d'informations, rendez vous sur <span class="lien">www.domisep.com </span>
+        </p>
+    </div>
 
+</br>
+
+</section>
 <?php
 
 require ('vues/footer.php');
