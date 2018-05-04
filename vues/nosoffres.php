a47217ecc1ba47ea4c1ae78cb483baba7a551085
Vincocote <vincent.pescio@yahoo.fr>
2018-05-04 15:42:31 +0200

    Nos Offres

--- vues/nosoffres.php
+++ vues/nosoffres.php
@@ -7,28 +7,57 @@
  */
 require ('vues/header.php');
 ?>
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="vues/CSS/nosoffres.css"/>
+</head>
 
-<section>
+
+<body>
     <h1>
         Nos Offres
     </h1>
-</section>
 
-<section>
-    <h2>
-        Nos Capteurs
-    </h2>
+    <section class="capteurs">
+        <h2>
+            Nos Capteurs
+        </h2>
 
-    <img src="">
+        <div class="lumierelegende"> <img class="capteurlumiere" src="ressources/pictures/capteurdelumiereLDR.jpg">
 
-    <P>
-        
-    </P>
+        <P>Capteur de lumière LDR</P>
+        </div>
+
+        <div class="temperaturelegende"> <img class="capteurtemperature" src="ressources/pictures/capteurdetemperature.JPG">
+
+        <p>
+            Capteur de température LM35
+        </p>
+        </div>
+
+        <div class="microlegende"> <img class="microelectret" src="ressources/pictures/microelectret.jpg">
+
+        <p>
+            Microphone electret
+        </p>
+        </div>
 
 
-</section>
+    </section>
 
-<section>
-    <h2> Nos Abonnements </h2>
+    <section class="subscription">
+        <h2> Nos Abonnements </h2>
 
-</section>
+        <div class="descriptionsubscription"> PACK UNE MAISON:<br/>
+            Installation des capteurs de votre choix dans une maison + 15 euros / mois pour l'utilisation de la plateforme<br/>
+
+
+            <br/>PACK PLUSIEURS MAISONS:<br/>
+            Installation des capteurs de votre choix dans plusieurs maisons + 20 euros/mois pour l'utilisation de la plateforme
+
+        </div>
+
+    </section>
+
+</body>
