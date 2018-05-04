03758ae47209c27e46132e9257dbad5151354eaa
Antoine <antoine.ap.57@gmail.com>
2018-05-04 15:36:20 +0200

    conditions utilisation

---
+++ controleurs/conditionsutilisation.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antoine
+ * Date: 04/05/2018
+ * Time: 15:34
+ */
+
+$section = 'conditionsutilisation';
+
+include('vues/conditionsutilisation.php');
