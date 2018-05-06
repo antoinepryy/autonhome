2260a99caf173c66abb8e1954ab22035d4998e8d
yacino97 <yacine.nabti97@gmail.com>
2018-05-07 01:46:58 +0200

    page données personnelles terminee

---
+++ vues/donneespersonnelles.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: yacin
+ * Date: 06/05/2018
+ * Time: 19:08
+ */
+
+require ('vues/header_'.$status.'.php');
+
+?>
+
+
+<div class="partiedonneespersonnelles">
+    <h1 class="titledonneespersonnelles">Données personnelles</h1>
+
+    <p class="textedonneespersonnelles">La loi de 1978 relative à l'informatique, aux fichiers et aux libertés, n'utilise pas la terminologie "
+        données personnelles " mais " informations nominatives " définies dans son article 4 : " les informations
+        qui permettent … l'identification des personnes physiques auxquelles elles s'appliquent ". La nouvelle
+        terminologie de " données personnelles " est employée par la directive du 24 octobre 1995 dans son article 2 :
+        " toute information concernant une personne physique identifiée et identifiable ". Les données personnelles
+        dans la définition extensive de la directive européenne, englobent tout ce qui se rapporte à l'identité de
+        la personne, à sa personnalité, à sa vie privée. Les données personnelles concernent donc nécessairement
+        les nom, prénom , adresse ; le téléphone, les coordonnées bancaires, le numéro de sécurité sociale ou NIR.
+        Une adresse électronique ou e-mail , une adresse IP ou adresse numérique de l'ordinateur sont des données
+        personnelles. <br/> <br/>
+        Par extension les données touchant à la famille, le patrimoine, la santé sont concernées. Les empruntes
+        génétiques, la voix, les images, doivent également être considérées comme des données personnelles. Il
+        faut citer le cas particulier de la vidéo-surveillance dont les enregistrements d' images et les sons
+        font l'objet d'une législation spéciale de la loi 95/73 du 21 Janvier 1995, sauf s'il y a constitution
+        de fichiers nominatifs. La CNIL considère par exemple que les vidéosurveillances par web-cam sont soumis
+        à la loi de 1978.</br><br/>
+        La loi de 1978 relative à l'informatique, aux fichiers et aux libertés, n'utilise pas la terminologie "
+        données personnelles " mais " informations nominatives " définies dans son article 4 : " les informations
+        qui permettent … l'identification des personnes physiques auxquelles elles s'appliquent ". La nouvelle
+        terminologie de " données personnelles " est employée par la directive du 24 octobre 1995 dans son article 2 :
+        " toute information concernant une personne physique identifiée et identifiable ". Les données personnelles
+        dans la définition extensive de la directive européenne, englobent tout ce qui se rapporte à l'identité de
+        la personne, à sa personnalité, à sa vie privée. Les données personnelles concernent donc nécessairement
+        les nom, prénom , adresse ; le téléphone, les coordonnées bancaires, le numéro de sécurité sociale ou NIR.
+        Une adresse électronique ou e-mail , une adresse IP ou adresse numérique de l'ordinateur sont des données
+        personnelles.
+    </p>
+</div>
+
+<div class="partiecadreassistanceFAQ">
+    <div class="cadreassistance">
+        <img class="logo_Assistance" src="ressources/pictures/logo_Assistance.png" title="logo_Assistance" alt="logo_Assistance">
+        <h2 class="titleassistance">Assistance</h2>
+        <p class="textecadreassistance">Une assistance ouverte 24h/24 et 7j/7 pour vous accompagner sur la prise en main et l'utilisation d'Autonhome</p>
+    </div>
+
+    <div class="cadreFAQ">
+        <img class="logo_FAQ" src="ressources/pictures/logo_FAQ.png" title="logo_FAQ" alt="logo_FAQ">
+        <h2 class="titleFAQ">FAQ</h2>
+        <p class="textecadreFAQ">La foire à questions d'Autonhome</p>
+    </div>
+</div>
+
+
+<?php
+
+require ('footer.php');
+?>
