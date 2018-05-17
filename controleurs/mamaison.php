cda7be26264f296e1a83d5a129e7ef43bb6083a2
Nigirith <fanny.streiff@gmail.com>
2018-05-17 09:58:59 +0200

    pieces

--- controleurs/mamaison.php
+++ controleurs/mamaison.php
@@ -49,24 +49,24 @@ if($status=='LU' && isset($_GET['id']) && isset($_GET['idroom'])){
     if(belongToUser($bdd, $_SESSION['userId'], $_GET['id'])){
         switch ($_GET['idroom']){
             case 1:
-                $section = 'salon';
-                include ('vues/salon.php');
-                break;
-            case 2 :
-                $section = 'cuisine';
-                include ('vues/cuisine.php');
-                break;
-            case 3 :
                 $section = 'chambre';
                 include ('vues/chambre.php');
+                break;
+            case 2 :
+                $section = 'salledebain';
+                include ('vues/salledebain.php');
+                break;
+            case 3 :
+                $section = 'salon';
+                include ('vues/salon.php');
                 break;
             case 4 :
                 $section = 'salleamanger';
                 include ('vues/salleamanger.php');
                 break;
             case 5 :
-                $section = 'salledebain';
-                include ('vues/salledebain.php');
+                $section = 'cuisine';
+                include ('vues/cuisine.php');
                 break;
             case 6 :
                 $section = 'autrespieces';
