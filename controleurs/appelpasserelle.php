2371cb1f3eb3ae08e2b589937ef2503d876f09bf
antoinepryy <antoine.ap.57@gmail.com>
2018-05-24 00:29:03 +0200

    add passerelle

--- controleurs/appelpasserelle.php
+++ controleurs/appelpasserelle.php
@@ -10,10 +10,20 @@ $ch = curl_init();
 curl_setopt(
     $ch,
     CURLOPT_URL,
-    "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+    "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=0002");
 curl_setopt($ch, CURLOPT_HEADER, FALSE);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
 curl_close($ch);
-echo "Raw Data:<br />";
-echo("$data");
+//echo "Raw Data:<br />";
+//echo("$data");
+
+$data_tab = str_split($data,33);
+
+for($i=0, $size=count($data_tab); $i<$size; $i++) {
+    echo "Trame $i: $data_tab[$i]<br />";
+}
+
+//list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
+//    sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+//echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
