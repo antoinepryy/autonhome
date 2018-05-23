117c555ae3c1fa74a567043ca93c780cbc614b0f
antoinepryy <antoine.ap.57@gmail.com>
2018-05-23 23:36:08 +0200

    passerelle

---
+++ controleurs/appelpasserelle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: antoi
+ * Date: 23/05/2018
+ * Time: 23:21
+ */
+
+$ch = curl_init();
+curl_setopt(
+    $ch,
+    CURLOPT_URL,
+    "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+curl_close($ch);
+echo "Raw Data:<br />";
+echo("$data");
