b27f9cc278bab5c1feb18de489d87b21cac598a1
antoinepryy <antoine.ap.57@gmail.com>
2018-05-29 23:54:50 +0200

    double join

--- modele/device.php
+++ modele/device.php
@@ -23,3 +23,29 @@ function findAllDevicesByRoom($bdd, $idRoom){
     return array($sensors->fetchAll(), $effectors->fetchAll());
 
 }
+
+function findAllDevicesByRoom2($bdd, $idroom){
+    $sensors = $bdd->prepare('
+    select name, state, value, dateTime, sensor_type.type from sensor
+    inner join
+    data on data.id_sensor = sensor.ID
+    inner join
+    sensor_type on sensor.id_sensorType = sensor_type.ID
+    where id_room = :idroom');
+    $sensors->bindParam(':idroom', $idroom);
+
+    $effectors = $bdd->prepare('
+    select name, action, state, effector_type.type from effector
+    inner join
+    effector_type on  effector_type.ID = effector.id_effectorType
+    where id_room = :idroom
+    ');
+    $effectors = $bdd->bindParam('idroom',$idroom);
+    $sensors->execute();
+    $effectors->execute();
+    return array($sensors->fetchAll(), $effectors->fetchAll());
+
+
+
+
+}
