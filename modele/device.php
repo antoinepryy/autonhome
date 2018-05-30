37178379f30e8eaa7893d7697541ac714b110e00
antoinepryy <antoine.ap.57@gmail.com>
2018-05-30 03:06:51 +0200

    requete complexe

--- modele/device.php
+++ modele/device.php
@@ -41,10 +41,15 @@ function findAllDevicesByRoom2($bdd, $idRoom){
     $sensors->bindParam(':idroom', $idRoom);
 
     $effectors = $bdd->prepare('
-    select name, action, state, effector_type.type from effector
+    select name, state, cardNumber, objectNumber value, dateTime, sensor_type.type from sensor
     inner join
-    effector_type on  effector_type.ID = effector.id_effectorType
-    where id_room = :idroom
+      (
+        SELECT MAX(dateTime) as dateTime, id_sensor
+        FROM data
+        GROUP BY id_sensor
+      ) as data
+    inner join
+    sensor_type on sensor.id_sensorType = sensor_type.ID
     ');
     $effectors->bindParam('idroom',$idRoom);
     $sensors->execute();
@@ -52,6 +57,18 @@ function findAllDevicesByRoom2($bdd, $idRoom){
     return array($sensors->fetchAll(), $effectors->fetchAll());
 
 
-
+//    SELECT data.ID,name, dateTime, data.value, state, cardNumber, objectNumber
+//FROM data
+//  INNER JOIN
+//    (
+//        Select MAX(dateTime) as latestDate, value, id_sensor
+//    FROM data
+//    Group by id_sensor
+//  ) max
+//    on data.dateTime = max.latestDate
+//    and data.id_sensor = max.id_sensor
+//  inner join sensor
+//    on data.id_sensor = sensor.ID
+//  inner join sensor_type on sensor.id_sensorType = sensor_type.ID;
 
 }
