82ddbd57fc295f4c7ea3b4d4e5b12a66e315f6e4
yacino97 <yacine.nabti97@gmail.com>
2018-05-31 10:50:37 +0200

    pour YM

--- controleurs/verifmysubscription.php
+++ controleurs/verifmysubscription.php
@@ -14,7 +14,7 @@ if(!function_exists("importAllSessionsAndModels")){
     importAllSessionsAndModels();
 }
 
-
+/*
 $status;
 if(isLoggedAsAdmin()){
     $status="AD";
@@ -26,23 +26,49 @@ else{
     $status="UU";
 
 }
-
+*/
 
 $infoUserAbonnement = findUserById($bdd, $_SESSION['userId']);
 
-joinIdSubscription;
 
 // faire un insert id dans user if il clique sur abonnement
-if(isset($_POST)){
+if(isset($_POST)) {
+    $section = 'monabonnement';
+    $status;
+
+    $status;
+    if (isLoggedAsAdmin()) {
+        $status = "AD";
+    } elseif (isLoggedAsUser()) {
+        $status = "LU";
+        updateUser($bdd, $_POST);
+        header("Location: index.php?cible=monabonnement");
+
+
+    } else {
+        require('erreur404.php');
+    }
+
+
+}
+
+
+
+    /*
 
 
     if($_POST['choixPackAbonnement']=='packUneMaison'){
+        subscription_user['id_subscription'] = 1;
 
 
     }
 
-};
+    else ($_POST['choixPackAbonnement']=='packUneMaison'){
 
+    }
+
+};
+*/
 header("Location: index.php?cible=......");
 ?>
 
@@ -50,3 +76,4 @@ if(isset($_POST["choixPackAbonnement"] ) ){
 //PackuneMaison
 //PackplusieursMaison
 };
+
