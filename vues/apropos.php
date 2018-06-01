d1619099df59abf5e1ba798c883bf7accc803a86
Nigirith <fanny.streiff@gmail.com>
2018-06-01 14:18:07 +0200

    a propos

--- vues/apropos.php
+++ vues/apropos.php
@@ -13,9 +13,9 @@ require ('vues/header_'.$status.'.php');
     <br/>
     <h1> À propos </h1>
     <div id="apropos">
-        <img class="logo" src="ressources/logo/domisep.GIF" alt="logo domisep"/>
+        <img class="logo" src="ressources/logo/DOMISEP.png" alt="logo domisep"/>
         <p class="text"> Autonhome est un service proposé par la sociéte Domisep, fondée en 2002. </br>
-            Pour plus d'informations, rendez vous sur <span class="lien">www.domisep.com </span>
+            Pour plus d'informations, rendez vous sur la page facebook de <a class="lien" href="https://www.facebook.com/Domisep-1204103683040258/"> Domisep </a>
         </p>
     </div>
 
