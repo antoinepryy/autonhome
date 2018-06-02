aa64f0e7c033bbcd9272f332678b21146f6d6aa1
antoinepryy <antoine.ap.57@gmail.com>
2018-06-02 21:06:01 +0200

    debut receiveframe

--- controleurs/AJAX/ReceiveFrame.php
+++ controleurs/AJAX/ReceiveFrame.php
@@ -5,3 +5,37 @@
  * Date: 01/06/2018
  * Time: 16:43
  */
+
+if(!function_exists("importAllSessionsAndModels")){ //Upload auto de toutes les variables de sessions ainsi que des fcts de BDD
+    include('importAllSessionsAndModels.php');
+    importAllSessionsAndModels();
+}
+$ch = curl_init();
+curl_setopt(
+    $ch,
+    CURLOPT_URL,
+    "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=G10B");
+curl_setopt($ch, CURLOPT_HEADER, FALSE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+$data = curl_exec($ch);
+curl_close($ch);
+//echo "Raw Data:<br />";
+//echo("$data");
+
+$data_tab = str_split($data,33);
+
+
+
+for($i=0, $size=count($data_tab); $i<$size; $i++) {
+    if(strlen($data_tab[$i]) == 33){
+        $statusFrame = readFrame($bdd, $data_tab[$i]);
+    }
+    else{
+
+        break;
+    }
+}
+
+
+
+
