998898c5e6b6c6ba467f7924f4a34f70f859891f
ameliep18 <memainef@gmail.com>
2018-06-03 17:38:51 +0200

    css forummessage

--- vues/forummessage.php
+++ vues/forummessage.php
@@ -11,16 +11,20 @@ require ('vues/header_'.$status.'.php');
 
 ?>
     <a href="index.php?cible=repondrediscussion&id=<?php echo($_GET["id"]) ?>"> <button class="repondre_discussion" type="submit" name="submit"> Répondre à la discussion </button> </a>
-        <?php foreach ($value as $element){ ?>
-        <h1> <?php echo($element["topic"]); ?> </h1>
-        <h2> <?php echo($element["firstName"]); ?>   <?php echo($element["lastName"]); ?> <?php echo($element["dateTime"]); ?> </h2>
-        <?php } ?>
-        <p><?php echo($element["text"]); ?></p>
 
-        <?php foreach ($text as $element){ ?>
-        <p> <?php echo($element[0]);?>  <?php echo($element[1]);?>  <?php echo($element[2]);?> </p>
+    <div class="topic"> <?php foreach ($value as $element){ ?>
+        <h2> <?php echo($element["topic"]); ?> </h2>
+            <h5> <?php echo($element["firstName"]); ?>   <?php echo($element["lastName"]); ?> </br> <span class="date"> <?php echo($element["dateTime"]); ?> </span></h5>
+        <?php } ?>
+        <p class="text"><?php echo($element["text"]); ?></p>
+        </div>
+
+        <div class="answers"> <?php foreach ($text as $element){ ?>
+                <p class="name"> <?php echo($element[0]);?>  <?php echo($element[1]);?> </p> <p class="date"> <?php echo($element[2]);?> </p>
         <p> <?php echo($element[3]);?> </p>
         <?php } ?>
+        </br>
+        </div>
 
 
 
