a60507337a710257db7f8c5404defb7c3ded58a1
ameliep18 <memainef@gmail.com>
2018-06-04 15:45:41 +0200

    faq

--- vues/faq.php
+++ vues/faq.php
@@ -17,30 +17,48 @@ require ('vues/header_'.$status.'.php');
 
 
         <p class="question">
-            <span class="question1"> Question 1 : </span> Comment s'inscrire ? <br>
+            Question 1 : Comment s'inscrire ? </br>
         </p>
 
         <p class="réponse">
-           Réponse 1 : Nam primum secundumque ac tertium Ahenobarborum Lucios, sequentis rursus tres ex ordine Gnaeos accepimus, reliquos non nisi uicissim tum Lucos tum Gnaeos.<br>
+           Pour vous inscire, cliquez sur le bouton "S'inscrire" sur la page principale.
+            </br>
+            Vous aurez à renseigner plusieurs champs, comme votre nom, votre prénom, votre adresse mail, votre téléphone, votre adresse de facturation ainsi que votre mot de passe.</br>
+            Enfin, en acceptant les Condition Générales d'Utilisation, votre compte client sera créé.</br>
         </p>
 
         <p class="question">
-            <span class="question2"> Question 2 :</span> Comment configurer mon compte ?<br>
+            Question 2 : Comment modifier mes informations personnelles ?</br>
         </p>
 
         <p class="réponse">
 
-           Réponse 2 : Pluris e familia cognosci referre arbitror, quo facilius appareat ita degenerasse a suorum uirtutibus Nero, ut tamen uitia cuiusque quasi tradita et ingentia rettulerit.<br>
+            Dans l'onglet Mon Profil, il vous est possible de modifier vos informations personnelles mais aussi de changer votre mot de passe.</br>
         </p>
 
         <p class="question">
-            <span class="question3"> Question 3 : </span> Comment payer ?  <br>
+            Question 3 : Comment puis-je ajouter une maison dont je suis propriétaire ?  </br>
         </p>
 
         <p class="réponse">
 
-           Réponse 3 : Ac ne praenomia quidem ulla praeterquam Gnaei et Luci usurparunt, eaque ipsa notabili uarietate, modo continuantes per singulas. <br>
+           Après avoir créé votre compte, rendez-vous dans l'onglet Ma Maison. Cliquez sur le bouton "Ajouter une maison".
+            </br>
+            Sélectionnez que vous en êtes le propriétaire.</br>
+            Vous devrez renseigner plusieurs informations comme son nom, son adresse ou encore la superficie et le nombre d'habitants.</br>
         </p>
+
+        <p class="question">
+            Question 4 : Comment puis-je ajouter une maison dont je suis locataire ?  </br>
+        </p>
+
+        <p class="réponse">
+
+            Après avoir créé votre compte, rendez-vous dans l'onglet Ma Maison. Cliquez sur le bouton "Ajouter une maison".</br>
+            Sélectionnez que vous en êtes le locataire.</br>
+            Vous devrez renseigner le code qui vous a été transmis par votre propriétaire.</br>
+        </p>
+
 
 
     </section>
