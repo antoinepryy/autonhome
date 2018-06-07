5d1bae3d5bf5bf1463f714e3cc2b6fccdc772de0
yacino97 <yacine.nabti97@gmail.com>
2018-06-07 15:06:04 +0200

    javascript mentionslegales

---
+++ vues/JS/mentionslegales.js
@@ -0,0 +1,30 @@
+function OnOff_1() {
+    if (document.getElementById("article1").style.display == "none")
+        document.getElementById("article1").style.display = "block";
+    else
+        document.getElementById("article1").style.display = "none";
+}
+function OnOff_2() {
+    if (document.getElementById("article2").style.display == "none")
+        document.getElementById("article2").style.display = "block";
+    else
+        document.getElementById("article2").style.display = "none";
+}
+function OnOff_3() {
+    if (document.getElementById("article3").style.display == "none")
+        document.getElementById("article3").style.display = "block";
+    else
+        document.getElementById("article3").style.display = "none";
+}
+function OnOff_4() {
+    if (document.getElementById("article4").style.display == "none")
+        document.getElementById("article4").style.display = "block";
+    else
+        document.getElementById("article4").style.display = "none";
+}
+function OnOff_5() {
+    if (document.getElementById("article5").style.display == "none")
+        document.getElementById("article5").style.display = "block";
+    else
+        document.getElementById("article5").style.display = "none";
+}
--- vues/mentionslegales.php
+++ vues/mentionslegales.php
@@ -14,6 +14,7 @@ require ('vues/header_'.$status.'.php');
     </br>
     <h1 class="title-mentionslegales"> MENTIONS LEGALES</h1>
     </br></br>
+
     <p class="intro">Conformément aux dispositions des Articles 6-III et 19 de la Loi n°2004-575 du 21 juin 2004
     pour la Confiance dans l&#39;économie numérique, dite L.C.E.N., il est porté à la connaissance des
     utilisateurs et visiteurs du site AutonHome.DomIsep.com les présentes mentions légales.
@@ -25,9 +26,10 @@ require ('vues/header_'.$status.'.php');
         de l&#39;internaute de toutes les dispositions des présentes Mentions Légales.</p>
     </br></br>
 
-    <h3>ARTICLE 1 – Informations légales</h3>
+
+    <input type="button" value="ARTICLE 1 – Informations légales" class="button" onclick="OnOff_1();"/>
     </br>
-    <p class="article1" >En vertu de l&#39;Article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans
+    <p id="article1" >En vertu de l&#39;Article 6 de la Loi n° 2004-575 du 21 juin 2004 pour la confiance dans
     l&#39;économie numérique, il est précisé dans cet article l&#39;identité des différents intervenants dans
     le cadre de sa réalisation et de son suivi.
     Le Site est édité par YMPIsep ayant son siège social à l&#39;adresse suivante : 28 rue Notre-Dame-
@@ -36,27 +38,33 @@ require ('vues/header_'.$status.'.php');
     utilisent le Site (ci-après « les Utilisateurs »).</p>
     </br></br>
 
-    <h3>ARTICLE 2 – Accessibilité</h3>
+
+
+    <input type="button" value="ARTICLE 2 – Accessibilité" class="button" onclick="OnOff_2();"/>
     </br>
-    <p class="article2" >Le Site est par principe accessible aux Utilisateurs 24/24h et 7/7j, sauf interruption,
+    <p id="article2" >Le Site est par principe accessible aux Utilisateurs 24/24h et 7/7j, sauf interruption,
     programmée ou non, pour des besoins de maintenance ou en cas de force majeure.
     En cas d&#39;impossibilité d&#39;accès au Site, celui-ci s&#39;engage à faire son maximum afin d&#39;en rétablir
     l&#39;accès. Le Site ne saurait être tenu pour responsable de tout dommage, quelle qu&#39;en soit la
         nature, résultant de son indisponibilité.</p>
     </br></br>
 
-    <h3>ARTICLE 3 – Collecte des données et loi informatiques et libertés</h3>
+
+
+    <input type="button" value="ARTICLE 3 – Collecte des données et loi informatiques et libertés" class="button" onclick="OnOff_3();"/>
     </br>
-    <p class="article3" >Ce site est déclaré à la Commission Nationale Informatique et Libertés (CNIL).
+    <p id="article3" >Ce site est déclaré à la Commission Nationale Informatique et Libertés (CNIL).
     En outre, il est conforme aux dispositions de la Loi 78-17 du 6 janvier 1978 relative à
     l&#39;informatique, aux fichiers et aux libertés. En vertu de celle-ci l&#39;Utilisateur bénéficie
     notamment d&#39;un droit d&#39;opposition (art. 32 et 38), d&#39;accès (art. 38 et 39) et de rectification (art.
         40) des données le concernant.</p>
     </br></br>
 
-    <h3>ARTICLE 4 – Politique de cookies</h3>
+
+
+    <input type="button" value="ARTICLE 4 – Politique de cookies" class="button" onclick="OnOff_4();"/>
     </br>
-    <p class="article4"> Le Site a éventuellement recours aux techniques de &quot;cookies&quot; lui permettant de traiter des
+    <p id="article4"> Le Site a éventuellement recours aux techniques de &quot;cookies&quot; lui permettant de traiter des
     statistiques et des informations sur le trafic, de faciliter la navigation et d&#39;améliorer le service
     pour le confort de l&#39;Utilisateur.
     Conformément à la législation européenne, le Site a mis à jour sa politique de confidentialité
@@ -64,15 +72,17 @@ require ('vues/header_'.$status.'.php');
         sites internet en modifiant les paramètres de son navigateur internet.</p>
     </br></br>
 
-    <h3>ARTICLE 5 – Contact</h3>
+
+
+    <input type="button" value="ARTICLE 5 – Contact" class="button" onclick="OnOff_5();"/>
     </br>
-    <p class="article5"> Pour tout signalement de contenus ou d&#39;activités illicites, l&#39;Utilisateur peut contacter le
+    <p id="article5"> Pour tout signalement de contenus ou d&#39;activités illicites, l&#39;Utilisateur peut contacter le
     Directeur de publication à l&#39;adresse suivante : autonhome@gmail.com, ou par courrier
-    recommandé avec accusé de réception adressé à l&#39;Éditeur aux coordonnées précisées dans les
-        présentes mentions légales.</p>
+    recommandé avec accusé de réception adressé à l&#39;Éditeur aux coordonnées précisées dans les présentes mentions légales.</p>
     </br></br></br>
 
 </section>
+
 
 <?php
 
