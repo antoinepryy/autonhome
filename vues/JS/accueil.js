aaf0bf506b15df35e8c54c7f9bc2869c2647c9e8
Vincocote <vincent.pescio@yahoo.fr>
2018-06-07 10:41:24 +0200

    Accueil AJAX

--- vues/JS/accueil.js
+++ vues/JS/accueil.js
@@ -1,12 +1,39 @@
 function lightRoomChange(id) {
-    console.log('ok');
-    var checkbox = document.getElementById(id);
+
+    var checkboxLight = document.getElementById("lightCheckbox");
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
 
         }
     }
-    xmlhttp.open("GET", "././controleurs/AJAX/SendFrame.php?command=modifyAll&idRoomType=6&&id=" + id + "&value=" + checkbox.checked, true);
+    xmlhttp.open("GET", "././controleurs/AJAX/SendFrame.php?command=modifyAllLight&id=" + id + "&value=" + checkboxLight.checked, true);
+    xmlhttp.send();
+}
+
+function fanRoomChange(id) {
+
+    var checkboxFan = document.getElementById("fanCheckbox");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+
+        }
+    }
+    xmlhttp.open("GET", "././controleurs/AJAX/SendFrame.php?command=modifyAllFan&id=" + id + "&value=" + checkboxFan.checked, true);
+    xmlhttp.send();
+}
+
+function shutterRoomChange(id) {
+
+    var checkboxShutter = document.getElementById("shutterCheckbox");
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+
+        }
+    }
+    xmlhttp.open("GET", "././controleurs/AJAX/SendFrame.php?command=modifyAllShutter&id=" + id + "&value=" + checkboxShutter.checked, true);
     xmlhttp.send();
 }
